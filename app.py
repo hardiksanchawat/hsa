df3daedded9833a8bafcde20382918c127765df4
Hardik Sanchawat <hardik.sanchawat@accenture.com>
2017-10-23 15:16:35 +0530

    first commit

--- app.py
+++ app.py
@@ -26,27 +26,27 @@ def webhook():
     print(res)
     r = make_response(res)
     r.headers['Content-Type'] = 'application/json'
+    print "jell>>",r
     return r
 
 def makeWebhookResult(req):
     if req.get("result").get("action") != "AHemophiliaHemophilia":
         return {}
     result = req.get("result")
-	parameters = result.get("parameters")
-    zone = parameters.get("qt")
+    # parameters = result.get("parameters")
+    # zone = parameters.get("qt")
     # For a Boto3 client.
-	ddb = boto3.client('dynamodb', aws_access_key_id='AKIAJKDZSPOYZ3A5KIHQ', aws_secret_access_key='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', region_name='us-east-2')
-	response = ddb.list_tables()
+    ddb = boto3.client('dynamodb', aws_access_key_id='AKIAJKDZSPOYZ3A5KIHQ', aws_secret_access_key='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', region_name='us-east-2')
+    response = ddb.list_tables()
 
-	# For a Boto3 service resource
-	dynamodb = boto3.resource('dynamodb', aws_access_key_id='AKIAJKDZSPOYZ3A5KIHQ', aws_secret_access_key='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', region_name='us-east-2')
+    # For a Boto3 service resource
+    dynamodb = boto3.resource('dynamodb', aws_access_key_id='AKIAJKDZSPOYZ3A5KIHQ', aws_secret_access_key='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', region_name='us-east-2')
 
     table = dynamodb.Table('medical_qa_details')
     response = table.scan(
-        FilterExpression=Attr('questions').eq(zone)
-    )
-	re = response['Items'][0]['answers']
-    speech = "answer of the question" + zone + "is" + str(re)
+        FilterExpression=Attr('questions').eq(result))
+    re = response['Items'][0]['answers']
+    speech = "answer of the question" + result + "is" + str(re)
     print("Response:")
     print(speech)
     return {
@@ -62,5 +62,6 @@ if __name__ == '__main__':
     port = int(os.getenv('PORT', 5000))
 
     print "Starting app on port %d" % port
-
     app.run(debug=True, port=port, host='0.0.0.0')
+    r = webhook()
+    makeWebhookResult(r)
