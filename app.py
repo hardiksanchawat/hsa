76758f2b3ce6717f0e94818be6967eee3d93efd4
Hardik Sanchawat <hardik.sanchawat@accenture.com>
2017-10-30 12:30:44 +0530

    initial commit

--- app.py
+++ app.py
@@ -16,36 +16,29 @@ app = Flask(__name__)
 @app.route('/webhook', methods=['POST'])
 def webhook():
     req = request.get_json(silent=True, force=True)
-    print ("r>>",req)
-    print("Request:")
     # req="What have hemophilia?"
     print(json.dumps(req, indent=4))
 
     res = makeWebhookResult(req)
-    print ("ffff>>>",res)
 
     res = json.dumps(res, indent=4)
     print(res)
     r = make_response(res)
-    r.headers['Content-Type'] = 'application/octet-stream'
-    print ("rt>>>",r)
+    r.headers['Content-Type'] = 'application/json'
     return r
 
 def makeWebhookResult(req):
-    print ("I am in")
-    print('m in>>',req)
     if req.get("result").get("action") != "AHemophiliaHemophilia":
         return {}
     result = req.get("result")
-    print ("result>>>",result)
     parameters = result.get("parameters")
     zone = parameters.get("qt")
     # For a Boto3 client.
-    ddb = boto3.client('dynamodb', aws_access_key_id='AKIAJ7DUJKCSBGJZHUCQ', aws_secret_access_key='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', region_name='us-east-2')
+    ddb = boto3.client('dynamodb', aws_access_key_id='', aws_secret_access_key='', region_name='')
     response = ddb.list_tables()
 
     # For a Boto3 service resource
-    dynamodb = boto3.resource('dynamodb', aws_access_key_id='AKIAJ7DUJKCSBGJZHUCQ', aws_secret_access_key='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', region_name='us-east-2')
+    dynamodb = boto3.resource('dynamodb', aws_access_key_id='', aws_secret_access_key='', region_name='')
 
     table = dynamodb.Table('medical_qa_details')
     response = table.scan(
